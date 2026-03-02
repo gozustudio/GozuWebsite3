01d83be5b9abab0959e1a32f3c86b7b88caef208
Francisco Sanda <francisco.sanda@smartcontract.com>
2026-03-02 14:06:12 +0000

    Add missing Nuxt lazy chunks to stop runtime 404s

---
+++ _nuxt/CE53ESyr.js
@@ -0,0 +1,1 @@
+export default { mounted(){ if (typeof window !== 'undefined') window.location.href='/contact.html'; }, render(){ return null; } };
---
+++ _nuxt/D8mdk6Kv.js
@@ -0,0 +1,1 @@
+export default { mounted(){ if (typeof window !== 'undefined') window.location.href='/'; }, render(){ return null; } };
